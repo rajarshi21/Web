32ea9a6105a7d8e3b27736a490db61cf587332b8
rajarshi21 <57459151+rajarshi21@users.noreply.github.com>
2022-08-16 00:28:11 +0530

    Added js file

---
+++ TestWebApp/scripts/calculate.js
@@ -0,0 +1,11 @@
+﻿var numberOfKeys = document.querySelectorAll("button").length
+for (var iKey = 0; iKey < numberOfKeys; iKey++)
+{
+    document.querySelectorAll("button")[iKey].addEventListener("click", function () {
+        var keyInnerHtml = this.innerHTML;
+        switch (keyInnerHtml)
+        {
+
+        }
+    })
+}
